8b3755e18a4e0ea880718a511e00984c5d535b94
Ishika Deshpande <ishikadeshpande816@gmail.com>
2025-07-08 22:03:09 +0530

    Update Register.jsx

--- gobook-frontend/src/components/Register/Register.jsx
+++ gobook-frontend/src/components/Register/Register.jsx
@@ -64,7 +64,7 @@ const Register = () => {
     <>
       <Navbar />
       <div className="flex flex-col items-center  justify-center min-h-screen bg-white bg-opacity-20 backdrop-blur-sm p-4">
-        <h2 className="text-2xl font-semibold mb-5 pt-30">Register for GoBook</h2>
+        <h2 className="text-2xl font-semibold mb-5 pt-28">Register for GoBook</h2>
 
         {errorMessage && (
           <p className="text-red-600 mb-4 text-sm text-center max-w-sm">{errorMessage}</p>
