a448d354d4d634c0d5d2fc557971c4e55b3a0e4c
Ishika Deshpande <ishikadeshpande816@gmail.com>
2025-07-09 17:09:14 +0530

    Update main.go

--- gobook-backend/main.go
+++ gobook-backend/main.go
@@ -37,7 +37,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://gobook-zk45.onrender.com"},
+		AllowOrigins:     []string{"https://gobook-zk45.onrender.com","https://www.hufd.xyz","https://hufd.xyz"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
